d35168e887117f12228099c0c40c832a6eae8f54
palpitate2015 <palpitate2015@gmail.com>
2020-05-13 23:30:09 +0900

    goldbach modify

--- 200513-goldbach.py
+++ 200513-goldbach.py
@@ -1,26 +1,33 @@
 # 소수 구하는 함수  
-def decimal(a):
-    if a % 2 != 0 and a % 3 != 0 and a**0.5 != int:
-        return True
-    elif a == 2:
-        return True
+def prime(a):
+    i = 2
+    while i < a/2:
+        if a % i == 0:
+            return False
+        elif a == 2:
+            return True
+        elif a == 3:
+            return True
+        else:
+            i = i + 1
+    return True
 
 # 정수 입력, 순서쌍 구하기
 N = int(input())
 a = int()
 b = int()
-decimal_list = []
+prime_list = []
 
 if 4 <= N <= 200000 and N % 2 == 0:
     for a in range(1, N):
         for b in range(1, N):
-            if decimal(a) is True and decimal(b) is True:
+            if prime(a) is True and prime(b) is True:
                 if a + b == N and a <= b:
-                    decimal_list.append((a, b))
+                    prime_list.append((a, b))
 
 # 순서쌍 출력
 try:
-print(decimal_list[0][0], decimal_list[0][1]) 
+    print(prime_list[0][0], prime_list[0][1]) 
 
 except:
     pass
